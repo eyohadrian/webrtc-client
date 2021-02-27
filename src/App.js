7498b76b1997a587426c374e79c9202ee313a003
Adrian <adrian.asensio.bdn@gmail.com>
2021-02-27 17:22:09 +0100

    screen share

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ function App({serverURI}) {
   const otherUser = useRef();
   const userStream = useRef();
   const roomId = "test";
+  const senders = useRef([]);
+
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
       userVideo.current.srcObject = stream;
       userStream.current = stream;
 
-      socketRef.current = io.connect(process.env.REACT_APP_SERVER_URI);
       socketRef.current = io.connect(serverURI);
       socketRef.current.emit("join room", roomId);
 
       socketRef.current.on('other user', userID => {
+        console.log("Remote User Joined - " + userID);
         callUser(userID);
         otherUser.current = userID;
       });
       socketRef.current.on("user joined", userID => {
+        console.log("User Joined - " + userID);
         otherUser.current = userID;
       });
       socketRef.current.on("offer", handleRecieveCall);
@@ -39,10 +42,11 @@ function App({serverURI}) {
 
   function callUser(userID) {
     peerRef.current = createPeer(userID);
-    userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+    userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
   }
 
   function createPeer(userID) {
+    console.log(`Create peer - ${userID}`)
     const peer = new RTCPeerConnection({
       iceServers: [
         {
@@ -61,7 +65,7 @@ function App({serverURI}) {
     return peer;
   }
   function handleNegotiationNeededEvent(userID) {
-    console.log("Negotiation");
+    console.log(`Negotiation - ${userID}`);
     peerRef.current.createOffer().then(offer => {
       return peerRef.current.setLocalDescription(offer);
     }).then(() => {
@@ -74,6 +78,7 @@ function App({serverURI}) {
     }).catch(e => console.log(e));
   }
   function handleRecieveCall(incoming) {
+    console.log(`Recive Call - ${JSON.stringify(incoming)}`)
     peerRef.current = createPeer();
     peerRef.current.setRemoteDescription(incoming.sdp).then(() => {
       userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
@@ -91,10 +96,12 @@ function App({serverURI}) {
     })
   }
   function handleAnswer(message) {
+    console.log(`Recive Call - ${JSON.stringify(message)}`)
     peerRef.current.setRemoteDescription(message.sdp).catch(e => console.log(e));
   }
 
   function handleICECandidateEvent(e) {
+    console.log(`Handle Ice Candidate - ${JSON.stringify(e)}`)
     if (e.candidate) {
       const payload = {
         target: otherUser.current,
@@ -104,11 +111,13 @@ function App({serverURI}) {
     }
   }
   function handleNewICECandidateMsg(incoming) {
+    console.log(`NewIceCandidate - ${JSON.stringify(incoming)}`)
     const candidate = new RTCIceCandidate(incoming);
     peerRef.current.addIceCandidate(candidate)
       .catch(e => console.log(e));
   }
   function handleTrackEvent(e) {
+    console.log(`Handle Track Event - ${JSON.stringify(e)}`)
     partnerVideo.current.srcObject = e.streams[0];
   }
   function hangout() {
@@ -125,16 +134,27 @@ function App({serverURI}) {
     userStream.current.getVideoTracks().forEach(x => x.enabled = !x.enabled)
   }
 
+  function shareScreen() {
+    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
+      const screenTrack = stream.getTracks()[0];
+      senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
+      screenTrack.onended = function() {
+        senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
+      }
+    })
+  }
+
   return (
     <Fragment>
       <div>
         <h1>Realtime communication with WebRTC</h1>
-        <video id="localVideo" autoPlay playsInline ref={userVideo} muted />
-        <video id="remoteVideo" autoPlay playsInline ref={partnerVideo}/>
+        <video style={{width: 720, height: 720}} id="localVideo" autoPlay playsInline ref={userVideo} muted />
+        <video style={{width: 720, height: 720}} id="remoteVideo" autoPlay playsInline ref={partnerVideo}/>
       </div>
       <div>
         <button onClick={mute}>Mute</button>
         <button onClick={hide}>Show</button>
+        <button onClick={shareScreen}>Share screen</button>
       </div>
     </Fragment>
   )
